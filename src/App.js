b2d5197192c8521ecee1f12c5908c6f9918badd1
Abbiyu Distira <abbiyukd@student.ce.undip.ac.id>
2020-11-22 12:33:30 +0700

    layout antd

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Layout } from "antd";
+import "./App.css";
+
+const { Header, Content, Footer } = Layout;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout className="layout">
+      <Header>Kelompok 8</Header>
+      <Content style={{ padding: "0 50px" }}>
+        <div className="site-layout-content">Content</div>
+      </Content>
+      <Footer style={{ textAlign: "center" }}>
+        Ant Design ©2018 Created by Ant UED
+ </Footer>
+    </Layout>
   );
 }
 
